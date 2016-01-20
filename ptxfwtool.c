b0d70b35c10f14bbe0a885604294371db9398e6b
Tao Wang <twang2218@gmail.com>
2016-01-20 16:00:26 +1100

    Format the code by `clang-format` using a style similar to LLVM style.
    The `.clang-format` file is included for the formatting. So, to avoid formatting problem next time, just format before commit.

--- ptxfwtool.c
+++ ptxfwtool.c
@@ -19,25 +19,21 @@
 //  Uncomment the line to enable debug info
 //#define DEBUG
 
-#define ENDIAN_BIG	0
-#define ENDIAN_LITTLE	1
+#define ENDIAN_BIG 0
+#define ENDIAN_LITTLE 1
 static unsigned int g_endian = ENDIAN_BIG;
 
 #ifndef min
-#define min(a,b) ((a)<(b)?(a):(b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
 #endif
 
-static unsigned int le16(const uint8_t *ptr)
-{
-	return (ptr[1] << 8) | ptr[0];
-}
-
+static unsigned int le16(const uint8_t *ptr) { return (ptr[1] << 8) | ptr[0]; }
 static unsigned int le32(const uint8_t *ptr)
 {
 	unsigned int res = 0;
 	res |= ptr[3] << 24;
 	res |= ptr[2] << 16;
-	res |= ptr[1] <<  8;
+	res |= ptr[1] << 8;
 	res |= ptr[0];
 
 	return res;
@@ -47,21 +43,17 @@ static void wle32(uint8_t *ptr, uint32_t v)
 {
 	ptr[3] = v >> 24;
 	ptr[2] = v >> 16;
-	ptr[1] = v >>  8;
+	ptr[1] = v >> 8;
 	ptr[0] = v;
 }
 
-static unsigned int be16(const uint8_t *ptr)
-{
-	return (ptr[0] << 8) | ptr[1];
-}
-
+static unsigned int be16(const uint8_t *ptr) { return (ptr[0] << 8) | ptr[1]; }
 static unsigned int be32(const uint8_t *ptr)
 {
 	unsigned int res = 0;
 	res |= ptr[0] << 24;
 	res |= ptr[1] << 16;
-	res |= ptr[2] <<  8;
+	res |= ptr[2] << 8;
 	res |= ptr[3];
 
 	return res;
@@ -71,7 +63,7 @@ static void wbe32(uint8_t *ptr, uint32_t v)
 {
 	ptr[0] = v >> 24;
 	ptr[1] = v >> 16;
-	ptr[2] = v >>  8;
+	ptr[2] = v >> 8;
 	ptr[3] = v;
 }
 
@@ -91,11 +83,7 @@ static void write32(uint8_t *ptr, uint32_t v)
 		return wbe32(ptr, v);
 }
 
-static void xor32(uint8_t *ptr, uint32_t v)
-{
-	write32(ptr, read32(ptr) ^ v);
-}
-
+static void xor32(uint8_t *ptr, uint32_t v) { write32(ptr, read32(ptr) ^ v); }
 static void load_iv(const uint8_t *ptr, uint32_t *iv)
 {
 	iv[0] = read32(ptr);
@@ -115,8 +103,8 @@ static void calculate_key(const uint32_t *iv, uint32_t *key)
 
 	for (unsigned int i = 0; i < 4; ++i) {
 		for (unsigned int j = 0; j < 4; ++j) {
-			key[i + 8*j + 0] = tmp[i] + iv[j] - 1;
-			key[i + 8*j + 4] = tmp[i] - iv[j] - 1;
+			key[i + 8 * j + 0] = tmp[i] + iv[j] - 1;
+			key[i + 8 * j + 4] = tmp[i] - iv[j] - 1;
 		}
 	}
 }
@@ -135,7 +123,7 @@ static int mangle_blocks(off_t offset, size_t len, void *bfr, const uint32_t *fu
 
 	while (blocks--) {
 		for (unsigned int i = 0; i < 0x20; ++i)
-			xor32(bfr + offset + 4*i, fullkey[i] - offset);
+			xor32(bfr + offset + 4 * i, fullkey[i] - offset);
 		offset += 0x80;
 	}
 
@@ -193,90 +181,90 @@ static size_t decompress(uint8_t *out, size_t out_len, uint8_t *in, size_t in_of
 
 	in += in_offs;
 	for (;;) {
-	if (out >= pstart_out + out_len) {
-		printf("!!! Missing stream end\n");
-		break;
-	}
-
-	blk_len = be16(in);
-	in += 2;
-#ifdef DEBUG
-	printf("Blk:%p len:%04X\n", in - (pend_in - in_len) - 2 + in_offs, blk_len);
-#endif
-
-	// end of stream
-	if (blk_len == 0)
-		break;
-
-	if (blk_len == 0xE000) { // special case
-		blk_len = (subtype ? 0x6000 : 0xC002);
-	} else if (blk_len > 0x8000) {
-		printf("!!! incorrect block length\n");
-		break;
-	}
-	if (in + blk_len + 2 + 2 > pend_in) {
-		printf("!!! input block is not complete\n");
-		break;
-	}
-
-	if (blk_len == 0xC002 && subtype == 0) { // raw block type 0
-		// copy and restart
-		memcpy(out, in, blk_len - 2);
-		// TODO what are last two bytes of such block (not zero) ?
-		out += (blk_len - 2);
-		in += blk_len;
-		continue;
-	} else if (blk_len == 0x6000 && subtype) { // raw block type 1
-		memcpy(out, in, blk_len);
-		out += blk_len;
-		in += blk_len;
-		continue;
-	}
-	// method appears based on LZRW-1A with different extendable length
-	mask = 0;
-	for (pend = in + blk_len - 2; in < pend;) {
-		if (!mask) {
-			map = be16(in);
-			in += 2;
-			mask = 0x8000;
-#ifdef DEBUG
-			printf("  Map %04X\n", map);
-#endif
-		} else {
-			if (map & mask) {	// COPY
-				int len = (in[0] & 7) + 3;
-				int offs = ((in[0] & 0xF8) << 5) | in[1];
-				in += 2;
-				// extended "length" field
-				if (len == 10)
-				do {
-					len += *in;
-				} while (*(in++) == 0xFF);
-#ifdef DEBUG
-				printf("  Mask %04X len:%d offs:%X I:%p, O:%p\n",
-					mask, len, offs, in - (pend_in - in_len) + in_offs, out - pstart_out);
-#endif
-				if (offs == 0) {
-					printf("!!! Zero offset\n");
-					break;
-				}
-
-				while ((len--) > 0) {
-					*out = *(out - offs);
-					out++;
-				}
-			} else {	// LITERAL
-				*(out++) = *(in++);
-			}
-			mask >>= 1;
+		if (out >= pstart_out + out_len) {
+			printf("!!! Missing stream end\n");
+			break;
 		}
-	}
-	// trailer
-	if (be16(in) != 0) {
-		printf("!!! Missing block end\n");
-		break;
-	}
-	in += 2;
+
+		blk_len = be16(in);
+		in += 2;
+#ifdef DEBUG
+		printf("Blk:%p len:%04X\n", in - (pend_in - in_len) - 2 + in_offs, blk_len);
+#endif
+
+		// end of stream
+		if (blk_len == 0)
+			break;
+
+		if (blk_len == 0xE000) { // special case
+			blk_len = (subtype ? 0x6000 : 0xC002);
+		} else if (blk_len > 0x8000) {
+			printf("!!! incorrect block length\n");
+			break;
+		}
+		if (in + blk_len + 2 + 2 > pend_in) {
+			printf("!!! input block is not complete\n");
+			break;
+		}
+
+		if (blk_len == 0xC002 && subtype == 0) { // raw block type 0
+			// copy and restart
+			memcpy(out, in, blk_len - 2);
+			// TODO what are last two bytes of such block (not zero)?
+			out += (blk_len - 2);
+			in += blk_len;
+			continue;
+		} else if (blk_len == 0x6000 && subtype) { // raw block type 1
+			memcpy(out, in, blk_len);
+			out += blk_len;
+			in += blk_len;
+			continue;
+		}
+		// method appears based on LZRW-1A with different extendable length
+		mask = 0;
+		for (pend = in + blk_len - 2; in < pend;) {
+			if (!mask) {
+				map = be16(in);
+				in += 2;
+				mask = 0x8000;
+#ifdef DEBUG
+				printf("  Map %04X\n", map);
+#endif
+			} else {
+				if (map & mask) { // COPY
+					int len = (in[0] & 7) + 3;
+					int offs = ((in[0] & 0xF8) << 5) | in[1];
+					in += 2;
+					// extended "length" field
+					if (len == 10)
+						do {
+							len += *in;
+						} while (*(in++) == 0xFF);
+#ifdef DEBUG
+					printf("  Mask %04X len:%d offs:%X I:%p, O:%p\n", mask, len, offs,
+					       in - (pend_in - in_len) + in_offs, out - pstart_out);
+#endif
+					if (offs == 0) {
+						printf("!!! Zero offset\n");
+						break;
+					}
+
+					while ((len--) > 0) {
+						*out = *(out - offs);
+						out++;
+					}
+				} else { // LITERAL
+					*(out++) = *(in++);
+				}
+				mask >>= 1;
+			}
+		}
+		// trailer
+		if (be16(in) != 0) {
+			printf("!!! Missing block end\n");
+			break;
+		}
+		in += 2;
 	}
 	return (out - pstart_out);
 }
@@ -303,46 +291,45 @@ int main(int argc, char *argv[])
 		g_endian = ENDIAN_BIG;
 		deobfuscate(in, 0, 0, 0x100, 0x0a00000 - 0x100);
 		deobfuscate(in, 0x0a00000, 0, 0x100, 0x80000 - 0x100);
-	} else if(strncmp((char *)in + 0xf24, "Copyright", 9) == 0
-		&& (in_size == 0xc00000 || in_size == 0xC40000)) {
+	} else if (strncmp((char *)in + 0xf24, "Copyright", 9) == 0 && (in_size == 0xc00000 || in_size == 0xC40000)) {
 		g_endian = ENDIAN_BIG;
 		deobfuscate(in, 0, 0xf00, 0, 0xf00);
 		deobfuscate(in, 0, 0xf00, 0x1000, 0xc00000 - 0x1000);
-        if (in_size >= 0xc3ff80)
-    		deobfuscate(in, 0xc00000, 0x3ff80, 0, 0x3ff80);
-	} else if(strncmp((char *)in + 0x124, "Copyright", 9) == 0) {
+		if (in_size >= 0xc3ff80)
+			deobfuscate(in, 0xc00000, 0x3ff80, 0, 0x3ff80);
+	} else if (strncmp((char *)in + 0x124, "Copyright", 9) == 0) {
 		g_endian = ENDIAN_LITTLE;
 		deobfuscate(in, 0, 0x100, 0, 0x100);
 		if (in_size > 0x2000020 && strncmp((char *)in + 0x2000010, "Copyright", 9) == 0)
-		    part_size = 0x2000000;
+			part_size = 0x2000000;
 		else
-		    part_size = 0x1000000;
+			part_size = 0x1000000;
 		deobfuscate(in, 0, 0x100, 0x200, part_size - 0x200);
 		deobfuscate(in, part_size, 0, 0x80, in_size - part_size - 0x80);
-	} else if(strncmp((char *)in, "PENTAX K-S", 10) == 0) {
-	    int subtype = *((uint8_t*)in+10)-'1';
-	    if (in_size&3) {
-    		fprintf(stderr, "!!! wrong file size\n");
-    		return -1;
-	    }
-	    uint32_t sum = 0;
-	    for (size_t i = 0; i<in_size; i+=4) {
-	        sum += le32(in+i);
-	    }
-	    if (sum) {
-    		fprintf(stderr, "!!! wrong checksum: %08X, expected 0\n", sum);
-    		return -1;
-	    }
-	    if (le16(in + 0x1F6) == 0) {
-    		fprintf(stderr, "!!! not compressed\n");
-    		return -1;
-	    }
-	    out_size = 0x2000000;
-	    out = malloc(out_size);
-	    memset (out, 0, out_size);
-	    /* limit is a position of resource files (0x00800000)
-	       0x005FFFFF is empirical limit for minimum of debug output in bad case */
-		out_size = decompress((uint8_t*)out, out_size, in, 0x200, min(in_size, 0x005FFFFF), subtype);
+	} else if (strncmp((char *)in, "PENTAX K-S", 10) == 0) {
+		int subtype = *((uint8_t *)in + 10) - '1';
+		if (in_size & 3) {
+			fprintf(stderr, "!!! wrong file size\n");
+			return -1;
+		}
+		uint32_t sum = 0;
+		for (size_t i = 0; i < in_size; i += 4) {
+			sum += le32(in + i);
+		}
+		if (sum) {
+			fprintf(stderr, "!!! wrong checksum: %08X, expected 0\n", sum);
+			return -1;
+		}
+		if (le16(in + 0x1F6) == 0) {
+			fprintf(stderr, "!!! not compressed\n");
+			return -1;
+		}
+		out_size = 0x2000000;
+		out = malloc(out_size);
+		memset(out, 0, out_size);
+		/* limit is a position of resource files (0x00800000)
+		   0x005FFFFF is empirical limit for minimum of debug output in bad case */
+		out_size = decompress((uint8_t *)out, out_size, in, 0x200, min(in_size, 0x005FFFFF), subtype);
 		free(in);
 		in = NULL;
 	} else {
@@ -359,6 +346,6 @@ int main(int argc, char *argv[])
 	fwrite(out, out_size, 1, fp);
 	fclose(fp);
 
-    free(out);
+	free(out);
 	return 0;
 }
